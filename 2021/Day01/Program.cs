ab364943a8cf4e85534a20370789dba47c0d3a00
Malthe Overgaard <malthe@plenge-overgaard.dk>
2021-12-02 20:48:14 +0100

    Day1

---
+++ 2021/Day01/Program.cs
@@ -0,0 +1,33 @@
+﻿List<int> depths = new List<int>();
+
+
+var lines = System.IO.File.ReadLines("./depths.txt");
+int increases = 0;
+for (int i = 1; i < lines.Count(); i++)
+{
+    if (int.Parse(lines.ElementAt(i-1)) < int.Parse(lines.ElementAt(i))) 
+    {
+        increases++;
+    }
+}
+
+Console.WriteLine("Task one equals = {0}", increases);
+
+int increasesV2 = 0;
+for (int i = 0; i < lines.Count(); i++)
+{
+    if (i == (lines.Count() - 3))
+    {
+        break;
+    }
+
+    int curr = int.Parse(lines.ElementAt(i)) + int.Parse(lines.ElementAt(i+1)) + int.Parse(lines.ElementAt(i+2));
+    int next = int.Parse(lines.ElementAt(i+1)) + int.Parse(lines.ElementAt(i+2)) + int.Parse(lines.ElementAt(i+3));
+
+    if (curr < next) 
+    {
+        increasesV2++;
+    }
+}
+
+Console.WriteLine("Task two equals = {0}", increasesV2);
