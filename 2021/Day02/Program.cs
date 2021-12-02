7543276b74361d9634feed3007c77b7456d488fa
Malthe Overgaard <malthe@plenge-overgaard.dk>
2021-12-02 20:59:20 +0100

    Day02

---
+++ 2021/Day02/Program.cs
@@ -0,0 +1,51 @@
+﻿int horizontalPosition = 0;
+int depthPosition = 0;
+int aim = 0;
+
+foreach (string line in System.IO.File.ReadLines("./commands.txt"))
+{
+    string[] commandArgs = line.Split(" ");
+    var length = int.Parse(commandArgs[1]);
+    switch (commandArgs[0])
+    {
+        case "forward":
+            horizontalPosition += length;
+            break;
+        case "down":
+            depthPosition += length;
+            break;
+        case "up":
+            depthPosition -= length;
+            break;
+        default:
+            break;
+    }
+}
+
+Console.WriteLine("Task one result = {0}", horizontalPosition*depthPosition);
+
+horizontalPosition = 0;
+depthPosition = 0;
+aim = 0;
+foreach (string line in System.IO.File.ReadLines("./commands.txt"))
+{
+    string[] commandArgs = line.Split(" ");
+    var length = int.Parse(commandArgs[1]);
+    switch (commandArgs[0])
+    {
+        case "forward":
+            horizontalPosition += length;
+            depthPosition += (aim * length);
+            break;
+        case "down":
+            aim += length;
+            break;
+        case "up":
+            aim -= length;
+            break;
+        default:
+            break;
+    }
+}
+
+Console.WriteLine("Task one result = {0}", horizontalPosition*depthPosition);
